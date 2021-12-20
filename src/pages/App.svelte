4242de863662d419b8f3ec791daf97a3acb1c521
toby <tobysx@gmail.com>
2021-12-20 12:04:43 +0000

    Added basic sign up component.

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -1,15 +1,52 @@
 <script>
+    import SignUp from "./SignUp.svelte";
 
 </script>
 
+<header class="header">
+    <h1 class="app-title">Reciply</h1>
+</header>
 <main>
-
+    <SignUp />
 </main>
 
+
 <style lang="scss">
-	@import "../css/variables";
+	@import "../css/bootstrap";
 
 	* {
 		font-family: $font-primary;
 	}
+
+    :global(body) {
+        @include flex($direction: column);
+
+        background: $color-primary;
+    }
+
+    .header {
+        @include flex($center: true);
+
+        box-sizing: border-box;
+        height: 190px;
+        padding-bottom: 30px;
+    }
+
+    .app-title {
+        color: $color-white;
+        font-size: 4.5rem;
+
+        :global(&) {
+            font-family: $font-title;
+        }
+    }
+
+    main {
+        background: $color-white;
+        border-radius: 16px 16px 0 0;
+        box-sizing: border-box;
+        flex-grow: 1;
+        margin-top: -30px;
+        padding: 1rem;
+    }
 </style>
---
+++ src/pages/SignUp.svelte
@@ -0,0 +1,63 @@
+<script>
+
+</script>
+
+<div class="sign-up">
+    <h1 class="title title--center">Sign Up</h1>
+
+    <form class="form">
+        <input
+            class="form__input"
+            type="text"
+            name="email"
+            placeholder="Email"
+        />
+        <input
+            class="form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+        />
+        <input
+            class="form__input"
+            type="password"
+            name="confirm_password"
+            placeholder="Confirm Password"
+        />
+
+        <input class="form__submit" type="submit" value="Sign Up" />
+    </form>
+
+    <div class="options">
+        <span>Already have an account? <a>Log In</a></span>
+    </div>
+</div>
+
+<style lang="scss">
+    @import "../css/bootstrap.scss";
+    @import "../css/forms.scss";
+
+    .sign-up {
+        @include flex($align: center, $justify: space-between, $direction: column);
+
+        height: 100%;
+    }
+
+    .form {
+        @include center;
+    }
+
+    .options {
+        a {
+            color: $color-grey;
+            cursor: pointer;
+            font-weight: bold;
+            transition: color .05s linear;
+
+            &:hover {
+                color: $color-primary;
+                text-decoration: none;
+            }
+        }
+    }
+</style>
