a8c40218b2c1c1a7d9ad4cd39c847c903ddd75ef
toby <tobysx@gmail.com>
2021-12-24 22:11:11 +0000

    Added start of shopping lists.

--- src/pages/ShoppingList.svelte
+++ src/pages/ShoppingList.svelte
@@ -17,12 +17,22 @@
 
 <h1 class="title">Shopping List</h1>
 
-<ProgressBar progress={progress} />
-
 {#if shoppingList && !error}
-    {#each shoppingList as list}
+    <ProgressBar progress={50} />
 
-    {/each}
+    <form class="shopping-lists form form--inline">
+        {#each shoppingList as list}
+            <fieldset class="list">
+                {#each list.ingredients as ingredient}
+                    <label class="form__checkbox">
+                        <input type="checkbox" bind:checked={ingredient.checked} />
+                        <span class="form__checkbox__check"></span>
+                        <span>{ingredient.name}</span>
+                    </label>
+                {/each}
+            </fieldset>
+        {/each}
+    </form>
 {:else if error}
     <Error>{error.message}</Error>
 {:else}
@@ -31,5 +41,21 @@
 
 <style lang="scss">
     @import "../css/bootstrap";
+    @import "../css/components/forms";
 
+    .shopping-lists {
+        @include flex($align: center, $justify: flex-start, $direction: column, $gap: 20px);
+
+        margin: 1.25rem 0;
+    }
+
+    .list {
+        background: $color-white-dark;
+        border: none;
+        border-radius: 4px;
+        box-sizing: border-box;
+        height: fit-content;
+        padding: .5rem;
+        width: 100%;
+    }
 </style>
