9622b4105855d9f5003c214496cd20f75a404c73
Toby Essex <tobysx@googlemail.com>
2022-05-22 18:14:12 +0100

    .

--- src/business/supabaseClient.js
+++ src/business/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = __api.env.SVELTE_APP_SUPABASE_URL
-const supabaseAnonKey = __api.env.SVELTE_APP_SUPABASE_ANON_KEY
+// TODO: FIX
+const supabaseUrl = __api.env.SVELTE_APP_SUPABASE_URL || "https://nrbsquegfdnrsjcfabyp.supabase.co"
+const supabaseAnonKey = __api.env.SVELTE_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTc0NjI3OSwiZXhwIjoxOTU1MzIyMjc5fQ.xkXfbAvVwRsrcz0on2Uj9kstj3WE8h_c7bo121E_PJI"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
